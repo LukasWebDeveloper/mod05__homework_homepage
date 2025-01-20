65402be0654a3568aa8fc7ee741aabf6da0efa45
Lukasz Duszak <lukas94.webdeveloper@gmail.com>
2025-01-20 18:26:04 +0100

    Update Commit

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
    
    let onChangeLogoPossition = () => {
       const logoElement = document.querySelector(".js-header__logo");
-      logoElement.classList.toggle("js-header__blockLogo--possition");
+      logoElement.classList.toggle("js-header__logoPosition");
    }
    
    
